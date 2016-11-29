59b0087e7a0293084ef6f3db3258910bb4f62926
Nikita Kakuev <nikita.kakuev@gmail.com>
2016-11-29 18:35:50 +0000

    filter only enabled or disabled options

--- list_cpp_warning_options.pl
+++ list_cpp_warning_options.pl
@@ -62,14 +62,64 @@ sub print_options
     print "\n";
 }
 
+sub get_cmd_arguments
+{
+    my %arguments;
+
+    foreach my $argument (@ARGV)
+    {
+        if    ($argument eq "--print-descriptions") { $arguments{"print_descriptions"} = 1;           }
+        elsif ($argument eq "--enabled-only")       { $arguments{"enabled_only"}       = 1;           }
+        elsif ($argument eq "--disabled-only")      { $arguments{"disabled_only"}      = 1;           }
+        elsif ($argument =~ /-W(no-)?/)             { push(@{$arguments{"gcc_warnings"}}, $argument); }
+        else                                        { die("Unknown argument: $argument");             }
+    }
+
+    if ($arguments{"enabled_only"} && $arguments{"disabled_only"})
+    {
+        die("Options `--enabled-only` and `--disabled-only` cannot be used together");
+    }
+
+    return %arguments;
+}
+
+sub filter_options
+{
+    my $arguments   = shift;
+    my $cpp_options = shift;
+
+    my $gcc_warnings = $arguments->{"gcc_warnings"}
+               ? join(" ", @{$arguments->{"gcc_warnings"}})
+               : "";
+    my $filter = $arguments->{"enabled_only"}  ? "enabled"
+               : $arguments->{"disabled_only"} ? "disabled"
+               : "";
+
+    my @lines          = split("\n", `gcc $gcc_warnings -Q --help=warnings`);
+    my @filtered_lines = grep(/$filter/, @lines);
+
+    my %filtered_options;
+    foreach my $line (@filtered_lines)
+    {
+        my $option = (split(" ", $line))[0];
+        if ($cpp_options->{$option})
+        {
+            $filtered_options{$option} = $cpp_options->{$option};
+        }
+    }
+
+    return %filtered_options;
+}
+
 sub main()
 {
-    my $print_descriptions = grep(/--print-descriptions/, @ARGV);
+    my %arguments = get_cmd_arguments();
 
-    my %options     = get_warning_options();
-    my %cpp_options = get_cpp_options(%options);
+    my %options          = get_warning_options();
+    my %cpp_options      = get_cpp_options(%options);
+    my %filtered_options = filter_options(\%arguments, \%cpp_options);
 
-    print_options(%cpp_options, $print_descriptions);
+    print_options(%filtered_options, $arguments{"print_descriptions"});
 }
 
 main();
